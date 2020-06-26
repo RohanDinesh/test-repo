f5c535129565847c4ffc0991aeaf2de6dcefa695
rohan <rohandinesh.blr@gmail.com>
2020-06-26 15:45:39 +0530

    2nd commit: byte size.java file added

--- src/test123/hello.java
+++ src/test123/hello.java
@@ -1,6 +1,6 @@
 package test123;
-
-public class Main {
+import java.util.*;
+public class hello {
 
     public static void main(String[] args) {
 	 System.out.println("How u doin'");
---
+++ src/test456/bytesize.java
@@ -0,0 +1,16 @@
+package test456;
+import java.util.*;
+
+public class bytesize {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter any number:");
+        int n = sc.nextInt();
+
+        System.out.println("Byte max: " +Byte.MAX_VALUE);
+        System.out.println("Char min: " + Character.MIN_VALUE+0);
+        System.out.println("Short max: " +Short.MAX_VALUE);
+    }
+
+}
